3ef33a4285f0a7e2b75984306bd0b5925d514828
Gerard Lim <gelim@Gerards-MacBook-Pro.local>
2024-09-07 16:36:49 +0800

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OrderModule } from './order/order.module';
-import { EmailModule } from './email/email.module';
 import configuration from './configuration';
 
 @Module({
   imports: [
     OrderModule,
-    EmailModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
